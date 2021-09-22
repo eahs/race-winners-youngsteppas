5dd7c6515dfc4985f98f7e8961eaab1608c07210
gabe j <jacobseng2003@gmail.com>
2021-09-22 11:31:36 -0400

    fixed your awful sort and calculating rank and printed places

--- RaceWinners/RaceWinners/Program.cs
+++ RaceWinners/RaceWinners/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Threading.Tasks;
+using RaceWinners.Models;
+using System.Collections.Generic;
 
 namespace RaceWinners
 {
@@ -19,8 +21,13 @@ namespace RaceWinners
 
                 Console.WriteLine($"{data[i].Name} - [{ranks}]");
             }
+            
+            Console.WriteLine($"A score:{data[0].GroupRank}");
+            Console.WriteLine($"B score:{data[1].GroupRank}");
+            Console.WriteLine($"C score:{data[2].GroupRank}");
+            Console.WriteLine($"D score:{data[3].GroupRank}");
 
-            for (int i = 1; i <= data.Count; i++)
+            /*for (int i = 1; i <= data.Count; i++)
             {
                 int hold = 0;
                 if (data[i - 1].GroupRank > data[i].GroupRank)
@@ -29,8 +36,24 @@ namespace RaceWinners
                     data[i].GroupRank = data[i - 1].GroupRank;
                     data[i - 1].GroupRank = hold;
                 }
+            } */
+            
+            for (int i = 1; i < data.Count; i++)
+            {
+                Group hold;
+                if (data[i - 1].GroupRank > data[i].GroupRank)
+                {
+                    hold = data[i];
+                    data[i] = data[i - 1];
+                    data[i - 1] = hold;
+                }
             }
-            Console.WriteLine($"{data[3].GroupRank}");
+            
+            Console.WriteLine($"4th place:{data[0].GroupRank}");
+            Console.WriteLine($"3rd place:{data[1].GroupRank}");
+            Console.WriteLine($"2nd place:{data[2].GroupRank}");
+            Console.WriteLine($"1st place:{data[3].GroupRank}");
+            
             
             
         }
